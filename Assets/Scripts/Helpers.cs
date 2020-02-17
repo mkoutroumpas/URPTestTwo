e9990504001439b5e8b2c90169e3ac8ad03e490b
Marios Koutroumpas <mkoutroumpas@gmail.com>
2020-02-17 07:38:30 +0100

    Initial commit.

---
+++ Assets/Scripts/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Mathematics;
+
+namespace Assets.Scripts.Classes
+{
+    public static class Helpers
+    {
+        public static void AddDummyHeavyTask(int length = 1000)
+        {
+            float value = 0f;
+            for (var i = 0; i < length; i++)
+            {
+                value = math.exp10(math.sqrt(value));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ZTranslationJob.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Classes;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+[BurstCompile]
+public struct ZTranslationJob : IJobParallelFor
+{
+    public NativeArray<float> MoveSpeeds;
+    public NativeArray<float3> Positions;
+
+    [ReadOnly]
+    public float DeltaTime;
+
+    [ReadOnly]
+    public bool MinusDirection;
+
+    public void Execute(int index)
+    {
+        Positions[index] += new float3(0f, 0f, (MinusDirection ? -1 : 1) * MoveSpeeds[index] * DeltaTime);
+        Helpers.AddDummyHeavyTask();
+    }
+}
