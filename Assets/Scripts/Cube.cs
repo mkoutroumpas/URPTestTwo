43d3510fe358305aae22344312248f0068a7293e
Marios Koutroumpas <mkoutroumpas@gmail.com>
2020-03-07 21:42:57 +0100

    Implement IConvertGameObjectToEntity in Cube prefab.

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class Cube : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager entityManager, GameObjectConversionSystem conversionSystem)
+    {
+        entityManager.AddComponent(entity, typeof(CubeComponent));
+        entityManager.AddComponentData(entity, new MoveSpeedComponentData { Value = Random.Range(0.1f, 1.0f) });
+    }
+}
