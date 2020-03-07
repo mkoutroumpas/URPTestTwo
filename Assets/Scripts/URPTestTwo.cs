3984a44b54424bbe17bb31700e6a8026c7959c81
Marios Koutroumpas <mkoutroumpas@gmail.com>
2020-03-07 21:13:57 +0100

    Do not keep state references to NativeArrays and implement ECS more clearly.

--- Assets/Scripts/URPTestTwo.cs
+++ Assets/Scripts/URPTestTwo.cs
@@ -1,21 +1,15 @@
-﻿using Assets.Scripts.Classes;
-using System.Collections.Generic;
-using Unity.Collections;
-using Unity.Entities;
-using Unity.Jobs;
-using Unity.Mathematics;
+﻿using Unity.Entities;
 using Unity.Transforms;
 using UnityEngine;
 
 public class URPTestTwo : MonoBehaviour
 {
-    private List<ZMoveableObject> _moveableObjects;
     private EntityManager _entityManager;
 
     [SerializeField]
     public bool UseJobSystem = true;
     [SerializeField]
-    public GameObject ObjectToMove;
+    public GameObject Prefab;
     [SerializeField]
     public int NumberOfObjects = 1000;
     [SerializeField]
@@ -27,83 +21,22 @@ public class URPTestTwo : MonoBehaviour
 
     private void Start()
     {
-        _moveableObjects = new List<ZMoveableObject>();
-
         var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
         _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-        var entity = GameObjectConversionUtility.ConvertGameObjectHierarchy(ObjectToMove, settings);
+        var entity = GameObjectConversionUtility.ConvertGameObjectHierarchy(Prefab, settings);
 
         for (var i = 0; i < NumberOfObjects; i++)
         {
             var position = new Vector3(
-                UnityEngine.Random.Range(-XSpread, XSpread),
-                UnityEngine.Random.Range(-YSpread, YSpread),
-                UnityEngine.Random.Range(-ZSpread, ZSpread));
+                Random.Range(-XSpread, XSpread),
+                Random.Range(-YSpread, YSpread),
+                Random.Range(-ZSpread, ZSpread));
 
             var entityInstance = _entityManager.Instantiate(entity);
+
+            _entityManager.AddComponent(entityInstance, typeof(Cube));
+            _entityManager.AddComponentData(entityInstance, new MoveSpeedComponentData { Value = Random.Range(0.1f, 1.0f) });
             _entityManager.SetComponentData(entityInstance, new Translation { Value = position });
-
-            _moveableObjects.Add(new ZMoveableObject
-            {
-                Entity = entityInstance,
-                Position = position,
-                Velocity = UnityEngine.Random.Range(0.1f, 1.0f)
-            });
         }
-    }
-
-    private void Update()
-    {
-        float startTime = Time.realtimeSinceStartup;
-
-        if (UseJobSystem)
-        {
-            var _velocities = new NativeArray<float>(_moveableObjects.Count, Allocator.TempJob);
-            var _positions = new NativeArray<float3>(_moveableObjects.Count, Allocator.TempJob);
-
-            for (var i = 0; i < _moveableObjects.Count; i++)
-            {
-                _positions[i] = _moveableObjects[i].Position;
-                _velocities[i] = _moveableObjects[i].Velocity;
-            }
-
-            var job = new ZTranslationJob
-            {
-                MoveSpeeds = _velocities,
-                Positions = _positions,
-                DeltaTime = Time.deltaTime,
-                MinusDirection = true
-            };
-
-            var jobHandle = job.Schedule(_moveableObjects.Count, 10);
-            jobHandle.Complete();
-
-            for (var i = 0; i < _moveableObjects.Count; i++)
-            {
-                _moveableObjects[i].Position = _positions[i];
-
-                _entityManager.SetComponentData(_moveableObjects[i].Entity, new Translation { Value = _moveableObjects[i].Position });
-            }
-            
-            _velocities.Dispose();
-            _positions.Dispose();
-        }
-        else
-        {
-            for (var i = 0; i < _moveableObjects.Count; i++)
-            {
-                _moveableObjects[i].Position += new Vector3(0f, 0f, -_moveableObjects[i].Velocity * Time.deltaTime);
-                Helpers.AddDummyHeavyTask();
-            }
-        }
-
-        Debug.Log(((Time.realtimeSinceStartup - startTime) * 1000f) + "ms");
-    }
-
-    public class ZMoveableObject
-    {
-        public Entity Entity;
-        public Vector3 Position;
-        public float Velocity;
     }
 }
---
+++ Assets/Scripts/ZTranslationSystem.cs
@@ -0,0 +1,38 @@
+﻿using Assets.Scripts.Classes;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class ZTranslationSystem : JobComponentSystem
+{
+    [BurstCompile]
+    [RequireComponentTag(typeof(Cube))]
+    struct TranslateJob : IJobForEach<Translation, MoveSpeedComponentData>
+    {
+        [ReadOnly]
+        public float DeltaTime;
+
+        [ReadOnly]
+        public bool MinusDirection;
+
+        public void Execute(ref Translation translation, ref MoveSpeedComponentData moveSpeed)
+        {
+            translation.Value += new float3(0f, 0f, (MinusDirection ? -1 : 1) * moveSpeed.Value * DeltaTime);
+            Helpers.AddDummyHeavyTask();
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new TranslateJob
+        {
+            MinusDirection = true,
+            DeltaTime = Time.DeltaTime
+        };
+
+        return job.Schedule(this, inputDeps);
+    }
+}
